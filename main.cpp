2ed726c5ff4b8e687b24f18a3207d9bafc75aabd
cocainedentist <cocainedentist0@gmail.com>
2025-09-23 09:50:40 +0900

    Picking Random Balls

--- main.cpp
+++ main.cpp
@@ -3,32 +3,29 @@
 #include <ctime>
 using namespace std;
 
-int ball = 0;
-int pickedBall[53] = { 0, };
-
-void ballPicker()
-{
-    bool task = true;
-    while (task)
-    {
-        ball = rand() % 52 + 1;
-        if (pickedBall[ball] == 0)
-        {
-            task = false;
-            pickedBall[ball] = 1;
-        }
-    }
-    cout << "Picked Ball: " << ball << endl;
-}
-
 int main() {
-    srand(time(NULL));
+    int pocket[53] = { 0, };
 
     for (int i = 0; i < 52; ++i)
     {
-        ballPicker();
+        pocket[i] = i + 1;
+    }
+    
+    for (int i = 0; i < 52 * 100; ++i)
+    {
+        int firstPick = rand() % 52;
+        int secondPick = rand() % 52;
+        int _temp = 0;
+
+        _temp = pocket[firstPick];
+        pocket[firstPick] = pocket[secondPick];
+        pocket[secondPick] = _temp;
     }
 
-    cout << "Completed!" << endl;
+    for (int i = 0; i < 52; ++i)
+    {
+        cout << pocket[i] << endl;
+    }
+
     return 0;
 }
