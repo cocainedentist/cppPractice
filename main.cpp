e39652c1fae83ee891941a9361911efc3b8ccd2c
cocainedentist <cocainedentist0@gmail.com>
2025-09-30 11:27:30 +0900

    BitOperator

--- main.cpp
+++ main.cpp
@@ -1,33 +1,28 @@
 ﻿#include <iostream>
+#include <bitset>
 
 using namespace std;
 
+#define WorldStatic					0b00000001
+#define WorldDynamic				0b00000010
+#define Pawn						0b00000100
+#define PhysicsBody					0b00001000
+								//  0b00000100 or
+								//  0b00000100 1> is true
 int main()
 {
-	////int Number[10] = { 0, 1, 2, 3, 4, 5 };
+	unsigned char MyActorType = Pawn | PhysicsBody;
+	string MyActorTypes = "Pawn, Physics";
+	if (MyActorType & Pawn)
+		if (MyActorType & Pawn)
+		{
+			cout << "I'm Pawn" << endl;
+		}
+		else
+		{
+			cout << "I'm not Pawn" << endl;
 
-	////int* P = Number;
-
-	//////cout << *(Number+1) << endl;
-	//////cout << Number[1] << endl;
-	//////cout << Number++ << endl;
-	////cout << *(++P) << endl;
-	////cout << *(P) << endl;
-
-
-	//int* P = new int[10];
-
-	//P[1] = 1;  //=>  *(P+1) = 1
-
-	//cout << *(P + 1) << endl;
-
-	int* P = new int;
-
-	delete P;
-
-	*P = 10;
-
-
+		}
 
 
 	return 0;
