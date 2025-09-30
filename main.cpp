bdcbcbc5a7fa6c5dfde5d602a254f96d3bc71468
cocainedentist <cocainedentist0@gmail.com>
2025-09-30 15:44:20 +0900

    Class Practice

--- main.cpp
+++ main.cpp
@@ -3,17 +3,48 @@
 using namespace std;
 
 
-class Image
+class State
 {
-	int X;
-	int Y;
-	int R;
-	int G;
-	int B; 
+public:
+	int ID;
+	string StateName;
 };
 
+class Transition
+{
+public:
+	int CurrentState;
+	string Condition;
+	int NextState;
+};
+
+class FSM
+{
+public:
+	FSM()
+	{
+
+	}
+	State States[4];
+	Transition Transitions[5];
+
+	void Transit(State InState, string Condition)
+	{
+
+	}
+};
 
 int main()
 {
-	
+	State States[4];
+	States[0].ID = 1;
+	States[0].StateName = "Patrol";
+	States[1].ID = 2;
+	States[1].StateName = "Chase";
+	States[2].ID = 3;
+	States[2].StateName = "Attack";
+	States[3].ID = 4;
+	States[3].StateName = "Death";
+
+	return 0;
 }
