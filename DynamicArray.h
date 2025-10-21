d78e7c79dc3b500a6dc5bf2e9c40d12816be2ae5
cocainedentist <cocainedentist0@gmail.com>
2025-10-21 17:16:03 +0900

    Iterator

--- DynamicArray.h
+++ DynamicArray.h
@@ -13,22 +13,42 @@ public:
 
 	virtual ~TDynamicArray()
 	{
-
+		if (Data)
+		{
+			delete[] Data;
+		}
 	}
 
+protected:
 	T* Data = nullptr;
 
-	int Size = 0;
-	int Capacity = 0;
+	size_t Size = 0;
+	size_t Capacity = 0;
 
-	int Index = 0;
+	int CurrentIndex = 0;
+
+public:
+	size_t GetSize()
+	{
+		return Size;
+	}
+
+	size_t GetCapacity()
+	{
+		return Capacity;
+	}
+
+	T& operator[](size_t _Index)
+	{
+		return Data[_Index];
+	}
 
 	void PushBack(T Value)
 	{
-		if (Index < Capacity) // 하나 삭제해도 된다. Index로 퉁칠꺼야 Size로 퉁칠꺼야 상관없음.
+		if (CurrentIndex < Capacity)
 		{
-			Data[Index] = Value;
-			Index++;
+			Data[CurrentIndex] = Value;
+			CurrentIndex++;
 			Size++;
 		}
 		else
@@ -38,33 +58,26 @@ public:
 			//[1][2][3][4][5]
 			//NewArray, Size = 1000000001
 			//[][][][][][]
-			/*
-			1. 크기를 * 2 만큼씩 늘려줄것이기 때문에 처음 Size가 0일 때 예외처리를 해야한다.
-			2. for문에서 Size만큼 이동하라고 명령, 하지만 처음이 원래 0이라서 안움직여야하는데 1로 바꿔줘서 오류가 남(움직일 곳이 없는데 움직이라함).
-			3. 그래서 공간 크기(Capacity)랑 자료 크기(Size)랑 따로 만들어준다.
-			*/
 			if (Capacity == 0)
 			{
 				Capacity = 1;
 			}
-			// 배열 사이즈 늘린다. = 새로운 배열을 만든다
 			int* NewArray = new int[Capacity * 2];
 
 			//memory copy
-			// 기존 배열에서 값 옮기기
-			for (int i = 0; i < Size; ++i)
-			{
-				NewArray[i] = Data[i];
-			}
-			//memcpy(Data, NewArray, Size * sizeof(int));
-			memmove(Data, NewArray, Size * sizeof(int));
+			//for (size_t i = 0; i < Size; ++i)
+			//{
+			//	NewArray[i] = Data[i];
+			//}
+
+			//memcpy(NewArray, Data, Size * sizeof(T));
+			memmove(NewArray, Data, Size * sizeof(int));
 
 			//NewArray, Size = 6
 			//[1][2][3][4][5][]
 
-			if (Data) // 지금은 처음 Data가 nullptr이기 때문에 Data가 존재할때만 지울 수 있도록 예외처리.
+			if (Data)
 			{
-				// 기존 배열 삭제
 				delete[] Data;
 			}
 			Data = NewArray;
@@ -72,8 +85,8 @@ public:
 			Size++;
 
 			//[1][2][3][4][5][6]
-			Data[Index] = Value;
-			Index++;
+			Data[CurrentIndex] = Value;
+			CurrentIndex++;
 		}
 	}
 };
--- main.cpp
+++ main.cpp
@@ -1,19 +1,57 @@
 #include "Engine.h"
 #include <iostream>
-#include <algorithm>
+
+//container
 #include <vector>
-#include "DynamicArray.h"
+#include <list>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
 
-int main()
+#include <algorithm>
+
+
+//class 설계
+//자료구조(STL) 사용 잘하냐
+
+int main(int argc, char* argv[])
 {
-	TDynamicArray<int> MyDynamicArray;
 
-	for (int i = 0; i < 18; i++)
+
+	std::vector<int> V;
+	V.push_back(1);
+	V.push_back(2);
+	V.push_back(3);
+
+	std::sort(V.begin(), V.end());
+	std::find(V.begin(), V.end(), 3);
+
+	//[][][]
+	//for (std::vector<int>::iterator i = V.begin() ; i != V.end(); ++i)
+	for (auto i = V.begin(); i != V.end(); ++i)
 	{
-		MyDynamicArray.PushBack(i);
+		std::cout << *i << ", ";
 	}
+	std::cout << std::endl;
 
-	MyDynamicArray.PrintArray();
+	std::list<int> L;
+	L.push_back(1);
+	L.push_back(2);
+	L.push_back(3);
 
-	return 0;
+	L.sort();
+	//std::find(L.begin(), L.end(), 3);
+
+	//c++ 14 -> c++ 17
+	//for (std::list<int>::iterator i = L.begin(); i != L.end(); ++i)
+	for (auto Data : L)
+	{
+		std::cout << Data << ", ";
+	}
+	std::cout << std::endl;
+
 }
+		
+ 
